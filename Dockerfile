4f975a391fc40194f0b2d58adb7ea8449ec8bc82
Tegar Wisnu Pambudi <sukandaru.japan@gmail.com>
2021-11-12 20:24:47 +0700

    edit image name on dockercompose

--- Dockerfile
+++ Dockerfile
@@ -45,3 +45,5 @@ RUN addgroup -g $uid -S $user \
 
 # Run supervisord
 CMD ["/usr/bin/supervisord", "-n", "-c" ,"/etc/supervisor/conf.d/supervisord.conf"]
+
+USER $user
--- src/scrap.sh
+++ src/scrap.sh
@@ -1,5 +1,40 @@
 #!/bin/sh
 
-curl -X "GET" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36" -o homepage.txt https://www.unila.ac.id
+actionScrap() {
+	curl \
+	-X "GET" \
+	-H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" \
+	-H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36" \
+	-o homepage.txt https://www.unila.ac.id \
+	--connect-timeout 30 \
+	--max-time 45 \
+	--silent
 
-php /var/www/admin/checkannouncements.php
+	RESULT=$?
+	echo ${RESULT}
+}
+
+actionGetAnnouncements() {
+	php /var/www/admin/checkannouncements.php
+}
+
+CHECKSCRAP="$(actionScrap)"
+if [[ ${CHECKSCRAP} -gt 0 ]]; then
+	RETRY=0
+	until [[ ${RETRY} -ge 3 ]]
+	do
+		RESULT="$(actionScrap)"
+		if [[ ${RESULT} -gt 0 ]]; then
+			echo "Retry ${RETRY}"
+			actionScrap
+			((RETRY++))
+		else
+			echo "Get Announcements...."
+			actionGetAnnouncements
+			break
+		fi
+	done
+else
+	echo "Get Announcements...."
+	actionGetAnnouncements
+fi
