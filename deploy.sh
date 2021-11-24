94f71a8339892b02dc21f78731a5c79ea55ed028
Tegar Wisnu Pambudi <sukandaru.japan@gmail.com>
2021-11-24 14:20:00 +0700

    Create deploy image on dev

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,6 @@
 
 clear
 
-cd ${WORK_DIR}
-
-echo "Checkout to dev branch"
-git checkout dev
-
-echo "Fetching branch dev"
-git fetch origin dev
-
-echo "Pulling branch dev"
-git pull origin dev
-IS_SUCCESS_UPDATE_PROJECT=$?
-if [[ ${IS_SUCCESS_UPDATE_PROJECT} -gt 0 ]]; then
-	echo "Failed Update Local Repo, Check Manually"
-	exit 1
-fi
-echo "Success update branch dev"
-
 if [[ "$1" == "down" ]]; then
 	echo "Down Service...."
 	docker-compose down --remove-orphans
@@ -28,10 +11,10 @@ if [[ "$1" == "down" ]]; then
 	fi
 elif [[ "$1" == "up" ]]; then
 	echo "Pull Image...."
-	docker-compose -f docker-compose.dev.yml pull -q
+	docker-compose -f ${COMPOSE_FILE} pull
 	sleep 1
 	echo "Up Service...."
-	docker-compose -f docker-compose.dev.yml up -d --remove-orphans
+	docker-compose -f ${COMPOSE_FILE} up -d --remove-orphans
 	ISSUCCESSUP=$?
 	if [[ ${ISSUCCESSUP} -gt 0 ]]; then
 		echo "Failed Up Image"
